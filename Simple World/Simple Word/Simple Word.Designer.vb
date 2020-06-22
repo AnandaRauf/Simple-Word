70c04b7d112fe6b7128213273a457d72be0a853b
AnandaRauf <uzumaki.nanda@rocketmail.com>
2020-06-22 21:37:13 +0700

    Developed by Tech Source Code Store

---
+++ Simple World/Simple Word/Simple Word.Designer.vb
@@ -0,0 +1,270 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class No1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OpenFileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NewFileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SaveToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SaveAsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.UndoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RedoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.QuitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutAppsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.TxtTulisBox = New System.Windows.Forms.RichTextBox()
+        Me.OpenFileDialogs = New System.Windows.Forms.OpenFileDialog()
+        Me.SaveFileDialogs = New System.Windows.Forms.SaveFileDialog()
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.ToolStripLabel1 = New System.Windows.Forms.ToolStripLabel()
+        Me.ToolStripDropDownButton2 = New System.Windows.Forms.ToolStripDropDownButton()
+        Me.TebalkanHurufBoldToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripLabel2 = New System.Windows.Forms.ToolStripLabel()
+        Me.ToolStripDropDownButton1 = New System.Windows.Forms.ToolStripDropDownButton()
+        Me.ImportGambarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.StatusFile = New System.Windows.Forms.StatusStrip()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.MenuStrip1.SuspendLayout()
+        Me.ToolStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.AboutAppsToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Padding = New System.Windows.Forms.Padding(10, 3, 0, 3)
+        Me.MenuStrip1.Size = New System.Drawing.Size(1370, 30)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.OpenFileToolStripMenuItem, Me.NewFileToolStripMenuItem, Me.SaveToolStripMenuItem, Me.SaveAsToolStripMenuItem, Me.UndoToolStripMenuItem, Me.RedoToolStripMenuItem, Me.QuitToolStripMenuItem})
+        Me.FileToolStripMenuItem.Font = New System.Drawing.Font("Century", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(50, 24)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'OpenFileToolStripMenuItem
+        '
+        Me.OpenFileToolStripMenuItem.Font = New System.Drawing.Font("Century", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.OpenFileToolStripMenuItem.Name = "OpenFileToolStripMenuItem"
+        Me.OpenFileToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.O), System.Windows.Forms.Keys)
+        Me.OpenFileToolStripMenuItem.Size = New System.Drawing.Size(242, 24)
+        Me.OpenFileToolStripMenuItem.Text = "Open File"
+        '
+        'NewFileToolStripMenuItem
+        '
+        Me.NewFileToolStripMenuItem.Name = "NewFileToolStripMenuItem"
+        Me.NewFileToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.N), System.Windows.Forms.Keys)
+        Me.NewFileToolStripMenuItem.Size = New System.Drawing.Size(242, 24)
+        Me.NewFileToolStripMenuItem.Text = "New File"
+        '
+        'SaveToolStripMenuItem
+        '
+        Me.SaveToolStripMenuItem.Name = "SaveToolStripMenuItem"
+        Me.SaveToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.S), System.Windows.Forms.Keys)
+        Me.SaveToolStripMenuItem.Size = New System.Drawing.Size(242, 24)
+        Me.SaveToolStripMenuItem.Text = "Save"
+        '
+        'SaveAsToolStripMenuItem
+        '
+        Me.SaveAsToolStripMenuItem.Name = "SaveAsToolStripMenuItem"
+        Me.SaveAsToolStripMenuItem.ShortcutKeys = CType(((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.Shift) _
+            Or System.Windows.Forms.Keys.S), System.Windows.Forms.Keys)
+        Me.SaveAsToolStripMenuItem.Size = New System.Drawing.Size(242, 24)
+        Me.SaveAsToolStripMenuItem.Text = "Save As"
+        '
+        'UndoToolStripMenuItem
+        '
+        Me.UndoToolStripMenuItem.Name = "UndoToolStripMenuItem"
+        Me.UndoToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.Z), System.Windows.Forms.Keys)
+        Me.UndoToolStripMenuItem.Size = New System.Drawing.Size(242, 24)
+        Me.UndoToolStripMenuItem.Text = "Undo"
+        '
+        'RedoToolStripMenuItem
+        '
+        Me.RedoToolStripMenuItem.Name = "RedoToolStripMenuItem"
+        Me.RedoToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.Y), System.Windows.Forms.Keys)
+        Me.RedoToolStripMenuItem.Size = New System.Drawing.Size(242, 24)
+        Me.RedoToolStripMenuItem.Text = "Redo"
+        '
+        'QuitToolStripMenuItem
+        '
+        Me.QuitToolStripMenuItem.Name = "QuitToolStripMenuItem"
+        Me.QuitToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Alt Or System.Windows.Forms.Keys.F4), System.Windows.Forms.Keys)
+        Me.QuitToolStripMenuItem.Size = New System.Drawing.Size(242, 24)
+        Me.QuitToolStripMenuItem.Text = "Quit"
+        '
+        'AboutAppsToolStripMenuItem
+        '
+        Me.AboutAppsToolStripMenuItem.Font = New System.Drawing.Font("Century", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.AboutAppsToolStripMenuItem.Name = "AboutAppsToolStripMenuItem"
+        Me.AboutAppsToolStripMenuItem.Size = New System.Drawing.Size(107, 24)
+        Me.AboutAppsToolStripMenuItem.Text = "About Apps"
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(61, 4)
+        '
+        'TxtTulisBox
+        '
+        Me.TxtTulisBox.Location = New System.Drawing.Point(0, 118)
+        Me.TxtTulisBox.Margin = New System.Windows.Forms.Padding(5)
+        Me.TxtTulisBox.Name = "TxtTulisBox"
+        Me.TxtTulisBox.Size = New System.Drawing.Size(2304, 1099)
+        Me.TxtTulisBox.TabIndex = 2
+        Me.TxtTulisBox.Text = ""
+        '
+        'OpenFileDialogs
+        '
+        Me.OpenFileDialogs.FileName = "OpenFileDialog1"
+        '
+        'ToolStrip1
+        '
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripLabel1, Me.ToolStripDropDownButton2, Me.ToolStripLabel2, Me.ToolStripDropDownButton1})
+        Me.ToolStrip1.Location = New System.Drawing.Point(0, 30)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.Size = New System.Drawing.Size(1370, 25)
+        Me.ToolStrip1.TabIndex = 3
+        Me.ToolStrip1.Text = "ToolStrip1"
+        '
+        'ToolStripLabel1
+        '
+        Me.ToolStripLabel1.Font = New System.Drawing.Font("Century", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ToolStripLabel1.Name = "ToolStripLabel1"
+        Me.ToolStripLabel1.Size = New System.Drawing.Size(41, 22)
+        Me.ToolStripLabel1.Text = "Home"
+        '
+        'ToolStripDropDownButton2
+        '
+        Me.ToolStripDropDownButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.ToolStripDropDownButton2.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.TebalkanHurufBoldToolStripMenuItem})
+        Me.ToolStripDropDownButton2.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripDropDownButton2.Name = "ToolStripDropDownButton2"
+        Me.ToolStripDropDownButton2.Size = New System.Drawing.Size(13, 22)
+        Me.ToolStripDropDownButton2.Text = "ToolStripDropDownButton2"
+        '
+        'TebalkanHurufBoldToolStripMenuItem
+        '
+        Me.TebalkanHurufBoldToolStripMenuItem.Font = New System.Drawing.Font("Century", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TebalkanHurufBoldToolStripMenuItem.Name = "TebalkanHurufBoldToolStripMenuItem"
+        Me.TebalkanHurufBoldToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.B), System.Windows.Forms.Keys)
+        Me.TebalkanHurufBoldToolStripMenuItem.Size = New System.Drawing.Size(245, 22)
+        Me.TebalkanHurufBoldToolStripMenuItem.Text = "Tebalkan huruf (Bold)"
+        '
+        'ToolStripLabel2
+        '
+        Me.ToolStripLabel2.Font = New System.Drawing.Font("Century", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ToolStripLabel2.Name = "ToolStripLabel2"
+        Me.ToolStripLabel2.Size = New System.Drawing.Size(38, 22)
+        Me.ToolStripLabel2.Text = "Tools"
+        '
+        'ToolStripDropDownButton1
+        '
+        Me.ToolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.ToolStripDropDownButton1.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ImportGambarToolStripMenuItem})
+        Me.ToolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripDropDownButton1.Name = "ToolStripDropDownButton1"
+        Me.ToolStripDropDownButton1.Size = New System.Drawing.Size(13, 22)
+        Me.ToolStripDropDownButton1.Text = "ToolStripDropDownButton1"
+        '
+        'ImportGambarToolStripMenuItem
+        '
+        Me.ImportGambarToolStripMenuItem.Font = New System.Drawing.Font("Century", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ImportGambarToolStripMenuItem.Name = "ImportGambarToolStripMenuItem"
+        Me.ImportGambarToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.G), System.Windows.Forms.Keys)
+        Me.ImportGambarToolStripMenuItem.Size = New System.Drawing.Size(209, 22)
+        Me.ImportGambarToolStripMenuItem.Text = "Import Gambar"
+        '
+        'StatusFile
+        '
+        Me.StatusFile.Location = New System.Drawing.Point(0, 728)
+        Me.StatusFile.Name = "StatusFile"
+        Me.StatusFile.Size = New System.Drawing.Size(1370, 22)
+        Me.StatusFile.TabIndex = 4
+        Me.StatusFile.Text = "StatusFile"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 67)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(87, 20)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Nama File"
+        '
+        'No1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1370, 750)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.StatusFile)
+        Me.Controls.Add(Me.ToolStrip1)
+        Me.Controls.Add(Me.TxtTulisBox)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Font = New System.Drawing.Font("Century", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Margin = New System.Windows.Forms.Padding(5)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "No1"
+        Me.Text = "Simple Word"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ToolStrip1.ResumeLayout(False)
+        Me.ToolStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OpenFileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents NewFileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SaveToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SaveAsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AboutAppsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents TxtTulisBox As RichTextBox
+    Friend WithEvents OpenFileDialogs As OpenFileDialog
+    Friend WithEvents SaveFileDialogs As SaveFileDialog
+    Friend WithEvents ToolStrip1 As ToolStrip
+    Friend WithEvents StatusFile As StatusStrip
+    Friend WithEvents UndoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents RedoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents QuitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripDropDownButton2 As ToolStripDropDownButton
+    Friend WithEvents TebalkanHurufBoldToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripLabel1 As ToolStripLabel
+    Friend WithEvents ToolStripLabel2 As ToolStripLabel
+    Friend WithEvents ToolStripDropDownButton1 As ToolStripDropDownButton
+    Friend WithEvents ImportGambarToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Label1 As Label
+End Class
---
+++ Simple World/Simple Word/Simple Word.vb
@@ -0,0 +1,103 @@
+﻿
+
+Public Class No1
+
+
+    Private Sub OpenFileToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OpenFileToolStripMenuItem.Click
+        Dim opf As New OpenFileDialog
+        Dim doc As String
+        opf.Filter = "(*.docx)|*.docx|(*.pdf)|*.pdf|(*.txt)|*.txt|All files (*.*)|*.*"
+        If opf.ShowDialog(Me) = DialogResult.OK Then
+            doc = opf.FileName
+            TxtTulisBox.Text = My.Computer.FileSystem.ReadAllText(doc)
+            MessageBox.Show("File sudah dibuka ^-^")
+            StatusFile.Text = "Nama file kamu :" + opf.FileName
+            Label1.Text = StatusFile.Text
+        End If
+    End Sub
+
+
+    Private Sub NewFileToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NewFileToolStripMenuItem.Click
+        Dim dfb As DialogResult = MessageBox.Show("Ingin buat file baru ?", "Buat file baru", MessageBoxButtons.YesNo)
+        If dfb = DialogResult.Yes Then
+            TxtTulisBox.SelectAll()
+            TxtTulisBox.SelectedText = ""
+            MessageBox.Show("Kamu membuat file baru ^-^")
+            StatusFile.Text = "Nama file kamu :" + ""
+            Label1.Text = StatusFile.Text
+        ElseIf dfb = DialogResult.No Then
+            Dim opf As New OpenFileDialog
+            StatusFile.Text = "Nama file kamu :" + opf.FileName
+            Label1.Text = StatusFile.Text
+        End If
+
+    End Sub
+
+    Private Sub SaveToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SaveToolStripMenuItem.Click
+        Dim svf As New SaveFileDialog
+        Dim doc As String
+        svf.Filter = "(*.docx)|*.docx|(*.pdf)|*.pdf|(*.txt)|*.txt|All files (*.*)|*.*"
+        If svf.ShowDialog(Me) = DialogResult.OK Then
+            doc = svf.FileName
+            My.Computer.FileSystem.WriteAllText(doc, TxtTulisBox.Text, True)
+            MessageBox.Show("Kamu telah save file ^-^")
+            StatusFile.Text = "Nama file kamu :" + svf.FileName
+            Label1.Text = StatusFile.Text
+        End If
+
+    End Sub
+
+
+    Private Sub SaveAsToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SaveAsToolStripMenuItem.Click
+        Dim svf As New SaveFileDialog
+        Dim doc As String
+        svf.Filter = "(*.docx)|*.docx|(*.pdf)|*.pdf|(*.txt)|*.txt|All files (*.*)|*.*"
+        If svf.ShowDialog(Me) = DialogResult.OK Then
+            doc = svf.FileName
+            My.Computer.FileSystem.WriteAllText(doc, TxtTulisBox.Text, True)
+            StatusFile.Text = "Kamu telah save file ^-^"
+            StatusFile.Text = "Nama file kamu :" + svf.FileName
+            Label1.Text = StatusFile.Text
+        End If
+    End Sub
+    Private Sub UndoToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles UndoToolStripMenuItem.Click
+        TxtTulisBox.Undo()
+    End Sub
+
+    Private Sub RedoToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles RedoToolStripMenuItem.Click
+        TxtTulisBox.Redo()
+    End Sub
+    Private Sub AboutAppsToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AboutAppsToolStripMenuItem.Click
+        MessageBox.Show("Dibuat oleh Tech Source Code Store pada tanggal 22 Juni 2020.")
+        MessageBox.Show("Terima kasih telah memakai aplikasi ini ^-^")
+    End Sub
+
+
+    Private Sub TebalkanHurufBoldToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles TebalkanHurufBoldToolStripMenuItem.Click
+        TxtTulisBox.SelectionFont = New Font(Me.TxtTulisBox.SelectionFont, FontStyle.Bold)
+    End Sub
+
+    Private Sub ImportGambarToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ImportGambarToolStripMenuItem.Click
+        Dim img As Image
+        Dim opimg As New OpenFileDialog
+        opimg.Filter = "(*.jpg)|*.jpg|(*.png)|*.png|(*.bmp)|*.bmp|All files (*.*)|*.*"
+        If opimg.ShowDialog(Me) = DialogResult.OK Then
+
+            img = Image.FromFile(opimg.FileName)
+            Clipboard.SetImage(img)
+            Me.TxtTulisBox.Paste()
+
+        End If
+
+
+    End Sub
+
+    Private Sub QuitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles QuitToolStripMenuItem.Click
+        Dim dfb As DialogResult = MessageBox.Show("Yakin ingin keluar , tanpa save file ?", "Keluar", MessageBoxButtons.YesNo)
+        If dfb = DialogResult.Yes Then
+            Close()
+        ElseIf dfb = DialogResult.No Then
+            Me.Show()
+        End If
+    End Sub
+End Class
